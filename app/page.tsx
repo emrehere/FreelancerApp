3b70c2213a5eb9f963436cb242607fcbf00203a3
emrehere <emrah.himself@hotmail.com>
2024-02-03 01:43:13 +0300

    latest

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,19 @@
 "use client";
 import React from "react";
-import Image from 'next/image'; // Import the Image component from Next.js
 
-import backgroundImage from '../public/ada1.jpg';
 
 const myBackgroundImage = '/ada1.jpg';
 
 const Page: React.FC = () => {
   return (
-    <div style={{ backgroundImage: `url(${myBackgroundImage})`, height: '100vh', width: '100vw', backgroundSize: 'cover' }} 
-     >
-    {/* Use the Image component with the src attribute */}
-      {/* Other components or content */}
+    <div  style={{ backgroundImage: `url(${myBackgroundImage})`, height: '100vh', width: '100vw', backgroundSize: 'cover' }} >
+      <div className="flex items-center  h-screen">
+      <div className="flex flex-col w-[40vw]  ml-[3vw] tracking-wide leading-relaxed
+       bg-blue-50 items-center -mt-[20vh] pl-2  rounded-3xl " >
+        <h1 className="text-5xl font-extrabold text-pink-600 p-4">Parlak fikirleri gerçekleştir</h1>
+        <h2 className="text-2xl text-gray-700 p-4 font-semibold ">1 milyon işletme tarafından güvenilen freelancer web sitesinde küresel yeteneklere erişim sağlayın.</h2>
+      </div>
+      </div>
     </div>
   );
 };
