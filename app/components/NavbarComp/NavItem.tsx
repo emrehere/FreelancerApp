7735fa8438f0ab9dfae940359ab8c6cfe8d72e21
emrehere <emrah.himself@hotmail.com>
2024-02-03 12:43:52 +0300

    latest

--- app/components/NavbarComp/NavItem.tsx
+++ app/components/NavbarComp/NavItem.tsx
@@ -1,6 +1,7 @@
 // NavItem.tsx
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
+
 
 
 interface NavItemProps {
@@ -14,8 +15,10 @@ const NavItem: React.FC<NavItemProps> = ({ title, href, icon }) => {
 
   
 
+  
+
   return (
-    <div className="text-slate-950 dark:text-slate-50 tracking-widest shadow-xl
+    <div  className="text-slate-950 dark:text-slate-50 tracking-widest shadow-xl
      bg-blue-100 px-2 py-2 bg-opacity-10 rounded-md  ">
       <div className='hover:text-orange-500'>
         <Link className='flex flex-row items-center space-x-2' href={href}>
