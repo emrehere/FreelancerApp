62948bcb6e9e702433a6bf92d841e06a117f2dd5
emrehere <emrah.himself@hotmail.com>
2024-02-08 08:33:55 +0300

    latest

--- app/components/NavbarComp/hamburgerMenu.tsx
+++ app/components/NavbarComp/hamburgerMenu.tsx
@@ -6,25 +6,27 @@ import { IoClose } from "react-icons/io5";
 
 
 export default function HamburgerMenu({ toggleMobileMenu }: any) {
-    return (
-        <div>
-            <div className="h-[120vh] w-[60vw] bg-blue-50 z-50 fixed top-0 right-0
+  return (
+    <div>
+      <div className="h-[120vh] w-[60vw] bg-blue-50 z-50 fixed top-0 right-0
             shadow-xl shadow-black " >
-                <IoClose onClick={toggleMobileMenu} size={50} className='absolute text-gray-500 -ml-[12vw] mt-[1vh]' />
-               <MobileSearchComp buttonName="ARA" />
-              <div>
-              <div className='flex flex-col   ' >
-        {
-          NavDatas.map((item: any) => {
-            return (
-              <MobileNavItems icon={item.icon} key={item.title} title={item.title} href={item.href} />
-            )
-          })
-        }
-      </div>
-              </div>
-            </div>
-
+        <IoClose onClick={toggleMobileMenu} size={50} className='absolute text-gray-500 -ml-[12vw] mt-[1vh]' />
+        <MobileSearchComp buttonName="ARA" />
+        <div>
+          <div className='flex flex-col   ' >
+            {
+              NavDatas.map((item: any) => {
+                return (
+                  <div onClick={toggleMobileMenu}>
+                    <MobileNavItems icon={item.icon} key={item.title} title={item.title} href={item.href} />
+                  </div>
+                )
+              })
+            }
+          </div>
         </div>
-    )
+      </div>
+
+    </div>
+  )
 }
