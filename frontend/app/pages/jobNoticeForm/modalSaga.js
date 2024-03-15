3d5ba586446d4561c5c47002fe9a06f0887d7a76
emrehere <emrah.himself@hotmail.com>
2024-03-15 13:20:05 +0300

    job notice posted succesfully

--- frontend/app/pages/jobNoticeForm/modalSaga.js
+++ frontend/app/pages/jobNoticeForm/modalSaga.js
@@ -1,5 +1,7 @@
 import { takeEvery,  call } from 'redux-saga/effects';
 
+
+
 function* hireInfoSaga(action) {
     console.log("triggered hireinfosaga")
     console.log("payload",action.payload)
@@ -7,35 +9,35 @@ function* hireInfoSaga(action) {
     const { hireInfo } = action.payload
 
     console.log("hireinfo in saga",hireInfo)
-  
+    const token = JSON.parse(localStorage.getItem('token'))
    
 
-    // try {
-    //     const res = yield call(fetch, 'http://localhost:8008/api/hire', {
-    //         method: 'POST',
-    //         headers: {
-    //             'Content-Type': 'application/json',
-    //             'Bearer': JSON.parse(localStorage.getItem('token')),
-    //         },
-    //         body: JSON.stringify({
-    //             hireInfo     
-    //         }),
-    //     });
+    try {
+        const res = yield call(fetch, 'http://localhost:8008/api/hire', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}` 
+            },
+            body: JSON.stringify({
+                hireInfo     
+            }),
+        });
 
-    //     console.log("my res", res)
+        console.log("my res", res)
 
-    //     if (res.ok) {
-    //         const data = yield res.json();
-    //         console.log("data", data)
-    //     } else {
+        if (res.ok) {
+            const data = yield res.json();
+            console.log("data", data)
+        } else {
             
-    //         const errorData = yield res.json(); 
-    //         console.log("error data", errorData)
-    //     }
-    // } catch (error) {
-    //     console.log('Error during login:', error);
+            const errorData = yield res.json(); 
+            console.log("error data", errorData)
+        }
+    } catch (error) {
+        console.log('Error during login:', error);
 
-    // }
+    }
 }
 
 export function* watchHireInfo() {
--- server/controllers/hireController.js
+++ server/controllers/hireController.js
@@ -3,27 +3,22 @@ import MyHireInfo from "../models/hireModel.js";
 
 async function createHire(req, res) {
        
-
-        console.log("req.user", req.user)
         const userId = req.user._id;
-        console.log("my userid in controller", userId);
+     
        
         try {
        
-                console.log("i m here")
-          const { unvan,soru, title, description, chosenCityToDB, chosenCountryToDB,
-        name, surname, phone } = req.body;
-         
-          const hireInfo = { unvan,soru, title, description, chosenCityToDB, chosenCountryToDB,
-        name, surname, phone };
-       
+              
+                console.log("req.body",req.body)
+          const { hireInfo } = req.body;
+ 
           
           const myHireInfoInstance = new MyHireInfo({  hireInfo, userId });
           console.log("myHireInfoInstance", myHireInfoInstance)
           await myHireInfoInstance.save();
           
       
-          res.status(201).json({ message: "lookingFor Data saved successfully" });
+          res.status(201).json({ message: "hireinfo Data saved successfully" });
         } catch (error) {
           console.error("Error saving data:", error);
           res.status(500).json({ error: "Internal Server Error" });
