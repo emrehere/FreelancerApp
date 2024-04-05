73248eb522127be79ba44a5b788acd8906007ed0
emrehere <emrah.himself@hotmail.com>
2024-04-05 18:46:27 +0800

    problem solved abput the sub categories

--- frontend/app/components/LoggedInNavbar/Categories.tsx
+++ frontend/app/components/LoggedInNavbar/Categories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setOpenState } from './categoryReducer';
 import { RootState } from '@/app/store/RootReducer';
@@ -6,72 +6,92 @@ import CategoryDatas from './CategoryDatas';
 
 function Categories() {
     const state = useSelector((state: RootState) => state.categoryReducer);
+    const [subHover, setSubHover] = useState(false);
     const dispatch = useDispatch();
 
-    console.log(CategoryDatas.tecnology)
+    console.log(state.open9);
+
+    console.log(subHover)
+
+    const closeSubHover = (category : string) => {
+        console.log("closeSubHover", category)
+        setSubHover(false);
+        dispatch(setOpenState({ category, isOpen: false }))
+    }
+
+    const openSubHover = (category : string) => {
+        console.log("openSubHover", category)
+        setSubHover(true);
+        dispatch(setOpenState({ category, isOpen: true }))
+    }
+
+    
+
+    
 
     return (
         <div className='z-20'>
             <div className='w-full bg-orange-50 flex justify-between px-[5vw] h-10  pt-2'>
                 <p  className='w-[10vw] h-full'
                     onMouseOver={() => dispatch(setOpenState({ category: 'open1', isOpen: true }))} 
-                    onMouseLeave={() => dispatch(setOpenState({ category: 'open1', isOpen: false }))}
+                    onMouseLeave={() => dispatch(setOpenState({ category: 'open1', isOpen : subHover ? true : false  }))}
                 >
                     Tech
                 </p>
                 <p 
                     className='w-[10vw] h-full' 
                     onMouseOver={() => dispatch(setOpenState({ category: 'open2', isOpen: true }))}
-                    onMouseLeave={() => dispatch(setOpenState({ category: 'open2', isOpen: false }))}
+                    onMouseLeave={() => dispatch(setOpenState({ category: 'open2', isOpen : subHover ? true : false  }))}
                 >
                     Writing
                 </p>
                 <p 
                     className='w-[10vw] h-full' 
                     onMouseOver={() => dispatch(setOpenState({ category: 'open3', isOpen: true }))}
-                    onMouseLeave={() => dispatch(setOpenState({ category: 'open3', isOpen: false }))}
+                    onMouseLeave={() => dispatch(setOpenState({ category: 'open3', isOpen : subHover ? true : false  }))}
                 >
                     Design
                 </p>
                 <p 
                     className='w-[10vw] h-full' 
                     onMouseOver={() => dispatch(setOpenState({ category: 'open4', isOpen: true }))}
-                    onMouseLeave={() => dispatch(setOpenState({ category: 'open4', isOpen: false }))}
+                    onMouseLeave={() => dispatch(setOpenState({ category: 'open4', isOpen : subHover ? true : false }))}
                 >
                     Marketing  
                 </p>
                 <p 
                     className='w-[10vw] h-full' 
                     onMouseOver={() => dispatch(setOpenState({ category: 'open5', isOpen: true }))}
-                    onMouseLeave={() => dispatch(setOpenState({ category: 'open5', isOpen: false }))}
+                    onMouseLeave={() => dispatch(setOpenState({ category: 'open5', isOpen : subHover ? true : false  }))}
                 >
                     Editing
                 </p>
                 <p 
                     className='w-[10vw] h-full' 
                     onMouseOver={() => dispatch(setOpenState({ category: 'open6', isOpen: true }))}
-                    onMouseLeave={() => dispatch(setOpenState({ category: 'open6', isOpen: false }))}
+                    onMouseLeave={() => dispatch(setOpenState({ category: 'open6', isOpen : subHover ? true : false  }))}
                 >
                     Business
                 </p>
                 <p 
                     className='w-[10vw] h-full' 
                     onMouseOver={() => dispatch(setOpenState({ category: 'open7', isOpen: true }))}
-                    onMouseLeave={() => dispatch(setOpenState({ category: 'open7', isOpen: false }))}
+                    onMouseLeave={() => dispatch(setOpenState({ category: 'open7', isOpen : subHover ? true : false  }))}
                 >
                     Audios
                 </p>
                 <p 
                     className='w-[10vw] h-full' 
                     onMouseOver={() => dispatch(setOpenState({ category: 'open8', isOpen: true }))}
-                    onMouseLeave={() => dispatch(setOpenState({ category: 'open8', isOpen: false }))}
+                    onMouseLeave={() => dispatch(setOpenState({ category: 'open8', isOpen : subHover ? true : false  }))}
                 >
                     Sales
                 </p>
                 <p 
                     className='w-[10vw] h-full' 
                     onMouseOver={() => dispatch(setOpenState({ category: 'open9', isOpen: true }))}
-                    onMouseLeave={() => dispatch(setOpenState({ category: 'open9', isOpen: false }))}
+                    onMouseLeave={() => dispatch(setOpenState({ category: 'open9', isOpen : subHover ? true : false  }))}
+                    
                 >
                     Media
                 </p>
@@ -80,14 +100,16 @@ function Categories() {
             {Object.keys(state).map(category => (
                 state[category] && (
                     <div 
-                        key={category}
-                        onMouseOver={() => dispatch(setOpenState({ category, isOpen: true }))} 
-                        onMouseLeave={() => dispatch(setOpenState({ category, isOpen: false }))}
-                        className='w-full z-100 bg-orange-50 flex text-sm  flex-wrap items-center pl-20 pt-4'
+                        onMouseEnter={ () => openSubHover(category)}
+                        onMouseLeave={() => closeSubHover(category)}
+                        key={category}                  
+                        className='w-full z-100 bg-orange-50 flex text-sm   flex-wrap items-center pl-20 '
                     >
                         {
+
                             category === 'open1' &&  CategoryDatas.tecnology.map(data => (
                                 <div 
+
                                     key={data}
                                     className=' h-full w-48 m-4'
                                 >
